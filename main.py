2d7b75896c814cae3d05e2e7db67fc88c164b78c
MohsinRaz3 <mohsinraz3@gmail.com>
2024-09-05 15:05:27 +0500

    type hint

--- main.py
+++ main.py
@@ -193,7 +193,7 @@ async def salad_transcript(audio_link: AudioLink = Body(...)):
         raise HTTPException(status_code=500, detail=f"An error occurred: {e}")
     
 @app.post("/micro_podcast/")
-async def create_micro_podcast(background_tasks:BackgroundTasks, audio_link: AudioLink = Body(...)):
+async def create_micro_podcast(background_tasks:BackgroundTasks, audio_link: AudioLink = Body(...))-> dict:
     try:
         background_tasks.add_task(call_bucket,audio_link.audio_link)
         return {"message": "success"}
@@ -207,6 +207,10 @@ async def create_micro_podcast(background_tasks:BackgroundTasks, audio_link: Aud
 @app.post("/micro_podcast_v2/")
 async def create_micro_podcast_v2(background_tasks:BackgroundTasks, podcast_data: PodcastData = Body(...)):
     try:
+        print("audio link", podcast_data.audio_link)
+        print("show notes prompt", podcast_data.show_notes_prompt)
+        print("podcast script prompt", podcast_data.show_notes_prompt)
+        
         background_tasks.add_task(call_bucket_v2,podcast_data.audio_link, podcast_data.show_notes_prompt, podcast_data.podcast_script_prompt)
         return {"message": "success"}
     
