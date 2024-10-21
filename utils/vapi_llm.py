815523998d6c2917660f2d6c094b70356a0166ea
MohsinRaz3 <mohsinraz3@gmail.com>
2024-10-21 05:54:50 +0500

    bgtask

--- utils/vapi_llm.py
+++ utils/vapi_llm.py
@@ -1,19 +1,21 @@
 from dotenv import load_dotenv
-import time
+import asyncio 
 import httpx
 import os
 
 load_dotenv()
+
 bearer_token = os.getenv('VAPI_BEARER_TOKEN')
 headers = {
     "Authorization": f"Bearer {bearer_token}"
 }
 url = "https://api.vapi.ai/call"
 
-async def get_vapi_data(call_id:str):
-    with httpx.AsyncClient() as client:
-        time.sleep(60) 
+async def get_vapi_data(call_id: str):
+    async with httpx.AsyncClient() as client:
+        await asyncio.sleep(60)  
+        
         response = await client.get(f"{url}/{call_id}", headers=headers)
+        
         text = response.json()
         print(text)
-
