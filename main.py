e28a1038a1ddc8bbd5f3b5c38df4c3b7199226a0
MohsinRaz3 <mohsinraz3@gmail.com>
2024-12-18 03:41:19 +0500

    endpoint updated

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from dotenv import load_dotenv
 from models import AudioLink, PodcastData, PodcastTextData, TextData
 from utils.mpodcast import call_bucket
 from utils.mpodcast_v2 import call_bucket_text_v2, call_bucket_v2, call_elevenlabs
-from utils.rocketprose import rocket_prose
 from utils.salad_transcription import salad_transcription_api
 from utils.search import scrape_website
 import httpx
@@ -467,10 +466,50 @@ async def transcribe_voice(file: UploadFile = File(...)):
 @app.post("/rocketprose_transcribe", tags=["Salad Trasncription API"])
 async def transcribe_rocketprose_voice(file: UploadFile = File(...)):
     """Takes audio blob file and creates transcription only"""
-    result = await rocket_prose(file=file)
-    return {"prose_transcription" : result}
-    
-    
+
+    try:
+        # Upload file to B2 storage
+        b2_file_url = await upload_b2_storage(file)
+
+        organization_name =  os.getenv('ORGANIZATION_NAME')
+        url = f"https://api.salad.com/api/public/organizations/{organization_name}/inference-endpoints/transcribe/jobs"
+        salad_key = os.getenv('SALAD_KEY')
+        language_code = "en"
+        list_of_audio_files = [b2_file_url]
+
+        list_of_job_ids = []
+        headers = {
+            "Salad-Api-Key": salad_key,
+            "Content-Type": "application/json",
+            "accept": "application/json",
+        }
+
+        for audio_file in list_of_audio_files:
+            data = {
+                "input": {
+                    "url": audio_file,
+                    "language_code": language_code,
+                    "word_level_timestamps": True,
+                    "diarization": True,
+                    "srt": True,
+                }
+            }
+            response = requests.post(url, headers=headers, json=data)
+            
+            #response.raise_for_status()
+            job_id = response.json()["id"]
+
+            list_of_job_ids.append(job_id)
+            get_transcription = await get_job(job_id)
+            if get_transcription:                
+                output_data = {"transcript" : get_transcription['output']['text']}
+                return output_data     
+            
+    except requests.exceptions.RequestException as e:
+        raise HTTPException(status_code=500, detail=f"Error during request: {e}")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"An error occurred: {e}")
+
 # if __name__ == "__main__":
 #     import uvicorn
 #     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
