bd69fdd87ee38fbbc6dfdd864bbb855d22cd4495
MohsinRaz3 <mohsinraz3@gmail.com>
2024-12-20 07:21:19 +0500

    endpoint model

--- main.py
+++ main.py
@@ -445,13 +445,11 @@ async def openai_advanced_custom_llm_route(request: Request):
     
     
 @app.post("/rocketprose_openaiapi", tags=["Salad Trasncription API"])
-async def transcription_response(proseData: Any):
+async def transcription_response(proseData: ProseRequest = Body(..., embed=True)):
     try:
-        print(f"heeeeeeeeeeeeeerer {proseData}" )
-
         print(f"transcribe value is '{proseData.transcribed_value}' and get text value '{proseData.text}'" )
-        # result = await transcription_prose(transcribed_value=proseData.transcribed_value,text=proseData.text)
-        return {"res":"result"}
+        result = await transcription_prose(transcribed_value=proseData.transcribed_value,text=proseData.text)
+        return result
     except Exception as e:
         print("Error:", str(e))
         return {"error": str(e)}
